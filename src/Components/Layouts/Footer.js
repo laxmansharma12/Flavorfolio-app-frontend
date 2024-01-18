bb3721db3d88c164fcb9b57593c65917d9dc1865
laxman <laxmansharmawe@gmail.com>
2024-01-18 17:08:22 +0530

    added social media icons to footer

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -3,16 +3,23 @@ import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { useAuth } from "../../context/authProvider";
+import { FaTwitter } from "react-icons/fa";
+import { TbBrandGithubFilled } from "react-icons/tb";
+import { FaLinkedinIn } from "react-icons/fa6";
 
 const FooterContainer = styled.div`
 	background-color: #000;
+	display: flex;
+	flex-direction: column;
+`;
+const FooterInnerContainer = styled.div`
 	display: flex;
 	color: #fff;
 	justify-content: center;
 	align-items: center;
 	gap: 5%;
 	flex-direction: row;
-	padding: 25px 40px;
+	padding: 40px;
 	@media (max-width: 1320px) {
 		gap: 0;
 	}
@@ -27,7 +34,7 @@ const TitleContainer = styled.div`
 	justify-content: center;
 	text-align: center;
 	align-items: center;
-	width: 20%;
+	width: 25%;
 	@media (max-width: 1320px) {
 		width: 25%;
 	}
@@ -91,6 +98,45 @@ const A = styled.a`
 		color: orange;
 	}
 `;
+
+const SocialMediaContainer = styled.div`
+	border: none;
+	border-top: 1px solid rgb(163, 154, 154);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	width: 75%;
+	margin: auto;
+	padding: 20px 0;
+	margin-bottom: 10px;
+	margin-top: 10px;
+	@media (max-width: 1320px) {
+		width: 85%;
+	}
+`;
+const IconsContainer = styled.div`
+	color: #fff;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 20px 0 15px 0;
+	gap: 15px;
+`;
+const Icons = styled.a`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: #fff;
+	border: 1px solid #fff;
+	padding: 10px;
+	border-radius: 50%;
+`;
+const P = styled.p`
+	margin-top: 0;
+	font-size: 14px;
+	color: #fff;
+`;
 export const Footer = () => {
 	const [auth, setAuth] = useAuth();
 	const navigate = useNavigate();
@@ -103,59 +149,78 @@ export const Footer = () => {
 	};
 	return (
 		<FooterContainer>
-			<TitleContainer>
-				<Title>Flavorfolio</Title>
-				<SubtTitle>
-					Explore the world of taste with us and savor the essence of culinary
-					excellence
-				</SubtTitle>
-			</TitleContainer>
-			<ListContainer>
-				<UL>
-					<LI>
-						<Link className="home" to={"/"}>
-							Home
-						</Link>
-					</LI>
-					<LI>
-						<A onClick={() => Scroll()}>Scroll Up</A>
-					</LI>
-				</UL>
-				<UL>
-					<LI>
-						<A
-							onClick={() => {
-								{
-									auth?.user
-										? navigate("/addrecipe")
-										: toast.error("Please Login To Add Recipe");
-								}
-							}}
-						>
-							AddRecipe
-						</A>
-					</LI>
-					<LI>
-						<A href="/myrecipe"> MyRecipe</A>
-					</LI>
-				</UL>
-				<UL className="unOrderedlist">
-					<LI>
-						<A href="/savedrecipe"> SavedRecipes</A>
-					</LI>
-					<LI>
-						<A href="/contact"> Contact</A>
-					</LI>
-				</UL>
-				<UL className="unOrderedlist">
-					<LI>
-						<A href="/login"> Login</A>
-					</LI>
-					<LI>
-						<A href="/register"> Register</A>
-					</LI>
-				</UL>
-			</ListContainer>
+			<FooterInnerContainer>
+				<TitleContainer>
+					<Title>Flavorfolio</Title>
+					<SubtTitle>
+						Explore the world of taste with us and savor the essence of culinary
+						excellence
+					</SubtTitle>
+				</TitleContainer>
+				<ListContainer>
+					<UL>
+						<LI>
+							<Link className="home" to={"/"}>
+								Home
+							</Link>
+						</LI>
+						<LI>
+							<A onClick={() => Scroll()}>Scroll Up</A>
+						</LI>
+					</UL>
+					<UL>
+						<LI>
+							<A
+								onClick={() => {
+									{
+										auth?.user
+											? navigate("/addrecipe")
+											: toast.error("Please Login To Add Recipe");
+									}
+								}}
+							>
+								AddRecipe
+							</A>
+						</LI>
+						<LI>
+							<A href="/myrecipe"> MyRecipe</A>
+						</LI>
+					</UL>
+					<UL className="unOrderedlist">
+						<LI>
+							<A href="/savedrecipe"> SavedRecipes</A>
+						</LI>
+						<LI>
+							<A href="/contact"> Contact</A>
+						</LI>
+					</UL>
+					<UL className="unOrderedlist">
+						<LI>
+							<A href="/login"> Login</A>
+						</LI>
+						<LI>
+							<A href="/register"> Register</A>
+						</LI>
+					</UL>
+				</ListContainer>
+			</FooterInnerContainer>
+			<SocialMediaContainer>
+				<IconsContainer>
+					<Icons
+						href={"https://www.linkedin.com/in/laxman-sharma/"}
+						target="display"
+					>
+						<FaLinkedinIn />
+					</Icons>
+					<Icons href={"https://twitter.com/laxmansharmaX"} target="display">
+						<FaTwitter />
+					</Icons>
+					<Icons href={"https://github.com/laxmansharma12"} target="display">
+						<TbBrandGithubFilled />
+					</Icons>
+				</IconsContainer>
+				<P>&copy;Copyright. All rights reserved</P>
+			</SocialMediaContainer>
 		</FooterContainer>
 	);
 };
